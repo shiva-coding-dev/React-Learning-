97907d37a42588ba6498a205b5d41308a9f5d8ac
Shiva Gupta <shivaiitpatna@gmail.com>
2025-08-29 02:45:19 +0530

    Changes the responsive UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react';
 
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS  , EXAMPLES} from './data.js';
 import Header from './components/header';
 import CoreConcepts from './components/CoreConcepts.jsx';
 import TabButtons from './components/tabButtons.jsx';
+
 function App() {
-  const [selectedTopic , setSelectedTopic] = useState("Please click a button");
+  const [selectedTopic , setSelectedTopic] = useState();
   function handleSelect(selectedButton ){
     setSelectedTopic(selectedButton);
   }
-
+  let tabContent = <p>Please Select a Topic</p>;
+  if(selectedTopic){
+    tabContent  = 
+    <div id = "tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>
+          {EXAMPLES[selectedTopic].code}
+        </code>
+      </pre>
+    </div>;
+  }
   return (
     <div>
       <Header/>
@@ -26,15 +39,14 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButtons onSelect={() => handleSelect('components')}>Components</TabButtons>
-            <TabButtons onSelect={() => handleSelect('jsx')}>JSX</TabButtons>
-            <TabButtons onSelect={() => handleSelect('props')}>Props</TabButtons>
-            <TabButtons onSelect={() => handleSelect('state')}>State</TabButtons>
+            <TabButtons isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButtons>
+            <TabButtons isSelected={selectedTopic === 'jsx' } onSelect={() => handleSelect('jsx')}>JSX</TabButtons>
+            <TabButtons isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButtons>
+            <TabButtons isSelected={selectedTopic === 'state' } onSelect={() => handleSelect('state')}>State</TabButtons>
             
           </menu>
+          {tabContent}
         </section>
-        {selectedTopic}
-        <h2>Time to get started!</h2>
       </main>
     </div>
   );
--- src/components/tabButtons.jsx
+++ src/components/tabButtons.jsx
@@ -1,8 +1,8 @@
-export default function TabButtons({children , onSelect}){
+export default function TabButtons({children , onSelect , isSelected}){
     
     return (
         <li>
-            <button onClick = {onSelect} >{children}</button>
+            <button className = {isSelected ? "active" : undefined} onClick = {onSelect} >{children}</button>
         </li>// chilren refers to the content between the Tabbutton used in the App 
     );
 }
